fe0e1f7af6c0d40132d54ae49cb02788a1d1af7c
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2020-09-07 05:17:21 +0200

    Stabilisation des test IT

--- parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -2,27 +2,42 @@ package com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.util.InputReaderUtil;
 
 public class FareCalculatorService {
+	
+	public  InputReaderUtil inputReaderUtil = new InputReaderUtil();
 
-    public void calculateFare(Ticket ticket){
+    public void calculateFare(Ticket ticket, boolean isRecurent){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        //TODO: Corréctions efféctuées !!
+       // On obtient le temps passé dans le parking en Millis
+       double duration = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
+       
+       //On divise le resultat par 3 600 000 qui equivaut à une heure en Millis afin de le convertir sur une base 1. 
+       double difference = duration / 3600000; 
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
-
+       //Mise á zéro de la difference pour permettre au parking d'etre gratuit si inferieur à 30mn
+       if(difference <= 0.5 ) {
+           difference = 0;
+       }
+       
+       if(isRecurent == true){
+    	   System.out.print(" MY BEAUTY CAR ");
+       }
+       
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+            	//Ajout d'une condtions ternaire afin d'ajouter 5% de reduction au utilisateur regulier.
+                ticket.setPrice((isRecurent == false) ? difference * Fare.CAR_RATE_PER_HOUR : difference * Fare.CAR_RATE_PER_HOUR * 0.95);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+            	//Ajout d'une condtions ternaire afin d'ajouter 5% de reduction au utilisateur regulier.
+                ticket.setPrice((isRecurent == false) ? difference * Fare.BIKE_RATE_PER_HOUR : difference * Fare.BIKE_RATE_PER_HOUR * 0.95);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
